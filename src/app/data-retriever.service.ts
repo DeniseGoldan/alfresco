ac6096c4f3fb15eadb2c6f8b873020df7b540788
Denise <denise.goldan@ness.com>
2018-03-29 11:10:58 +0300

    Added service method for extracting endpoint response body

--- src/app/data-retriever.service.ts
+++ src/app/data-retriever.service.ts
@@ -3,25 +3,31 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 import { Injectable } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DataRetrieverService {
 
+	private endpointUrl: string = "https://s3.eu-west-2.amazonaws.com/alfresco-adf-app-test/favorites.json";
+
 	constructor(private http: HttpClient) {}
 
-	dummy(): Promise<any> {
-		return this.http.get('https://s3.eu-west-2.amazonaws.com/alfresco-adf-app-test/favorites.json', {observe: 'response'})
-		// return this.http.get('http://172.30.146.173:8080/api/qds/v1/suites/applications-summary', {observe: 'response'})
+	public getEntriesArray(): Promise<any> {
+		return this.getResponseBody()
+		.then(responseBody => {return responseBody.list.entries})
+		.catch(error => {
+			return error;
+		})
+	}
+
+	private getResponseBody(): Promise<any> {
+		return this.http.get(this.endpointUrl, {observe: "response"})
 		.toPromise()
 		.then(response => {
-			return response;
-			// return "AAAAAAAAAAAAAAAA";
+			return response.body;
 		})
 		.catch(error => {
 			return error;
-			// return "BBBBBBBBBBBBBBB";
 		})
 	}
 
--- src/app/favourite-items.component.ts
+++ src/app/favourite-items.component.ts
@@ -12,7 +12,7 @@ export class FavouriteItemsComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.dataRetrieverService
-		.dummy()
+		.getEntriesArray()
 		.then(result => console.log(JSON.stringify(result)))
 		.catch(error => console.log(JSON.stringify(error)));
 	}
